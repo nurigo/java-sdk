46ed8ca276da6c5fdc21b4f6a7ccf192c1174d7d
Subin Lee <chandar.sblee@gmail.com>
2022-04-26 18:35:11 +0900

    Change build.gradle.kts:
    * Remove test dependency and test task
    * Add jar additional task
    * Upgrade kotlin versions

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,39 +2,35 @@ import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
     `java-library`
-    kotlin("jvm") version "1.6.0"
-    kotlin("plugin.serialization") version "1.6.0"
-    id("org.jetbrains.dokka") version "1.6.0"
+    kotlin("jvm") version "1.6.21"
+    kotlin("plugin.serialization") version "1.6.21"
+    id("org.jetbrains.dokka") version "1.6.21"
     `maven-publish`
     signing
 }
 
 group = "net.nurigo"
-version = "4.1.3"
+version = "4.1.4"
 
 repositories {
     mavenCentral()
 }
 
 dependencies {
-    testImplementation("org.junit.jupiter:junit-jupiter-api:5.8.1")
-    testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.8.1")
-    implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8:1.6.0")
-    implementation("commons-codec:commons-codec:1.15")
-    implementation("org.jetbrains.kotlinx:kotlinx-datetime-jvm:0.3.1")
+    compileOnly("org.jetbrains.kotlin:kotlin-stdlib-jdk8:1.6.21")
+    compileOnly("commons-codec:commons-codec:1.15")
+    compileOnly("org.jetbrains.kotlinx:kotlinx-datetime-jvm:0.3.2")
 
-    implementation(platform("com.squareup.okhttp3:okhttp-bom:4.9.3"))
+    compileOnly("com.squareup.okhttp3:okhttp:4.9.3")
+    compileOnly("com.squareup.okhttp3:logging-interceptor:4.9.3")
 
-    implementation("com.squareup.okhttp3:okhttp")
-    implementation("com.squareup.okhttp3:logging-interceptor")
+    compileOnly("com.squareup.retrofit2:retrofit:2.9.0")
+    compileOnly("org.jetbrains.kotlinx:kotlinx-serialization-json:1.3.2")
+    compileOnly("com.jakewharton.retrofit:retrofit2-kotlinx-serialization-converter:0.8.0")
 
-    implementation("com.squareup.retrofit2:retrofit:2.9.0")
-    implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.3.1")
-    implementation("com.jakewharton.retrofit:retrofit2-kotlinx-serialization-converter:0.8.0")
-
-    dokkaHtmlPlugin("org.jetbrains.dokka:kotlin-as-java-plugin:1.6.0")
-    dokkaHtmlPlugin("org.jetbrains.dokka:javadoc-plugin:1.6.0")
-    implementation("org.jetbrains.kotlin:kotlin-reflect:1.6.0")
+    dokkaHtmlPlugin("org.jetbrains.dokka:kotlin-as-java-plugin:1.6.20")
+    dokkaHtmlPlugin("org.jetbrains.dokka:javadoc-plugin:1.6.20")
+    compileOnly("org.jetbrains.kotlin:kotlin-reflect:1.6.0")
 }
 
 java {
@@ -46,14 +42,30 @@ tasks.getByName<Test>("test") {
     useJUnitPlatform()
 }
 
+
 val compileKotlin: KotlinCompile by tasks
 compileKotlin.kotlinOptions {
     jvmTarget = "1.8"
 }
-val compileTestKotlin: KotlinCompile by tasks
+
+tasks.withType<Jar> {
+    // Otherwise you'll get a "No main manifest attribute" error
+    manifest {
+        attributes["Main-Class"] = "net.nurigo.sdk"
+    }
+
+    // To add all of the dependencies
+    from(sourceSets.main.get().output)
+
+    dependsOn(configurations.runtimeClasspath)
+    from({
+        configurations.runtimeClasspath.get().filter { it.name.endsWith("jar") }.map { zipTree(it) }
+    })
+}
+/*val compileTestKotlin: KotlinCompile by tasks
 compileTestKotlin.kotlinOptions {
     jvmTarget = "1.8"
-}
+}*/
 
 tasks.dokkaHtml.configure {
     outputDirectory.set(buildDir.resolve("dokka"))
