6c8dd5640828c2409b7b02c7c1918b92b382a452
Subin Lee <chandar.sblee@gmail.com>
2022-05-24 22:29:11 +0900

    Update DefaultAgent.kt

--- src/main/java/net/nurigo/sdk/message/request/DefaultAgent.kt
+++ src/main/java/net/nurigo/sdk/message/request/DefaultAgent.kt
@@ -4,6 +4,6 @@ import kotlinx.serialization.Serializable
 
 @Serializable
 data class DefaultAgent(
-    val sdkVersion: String = "java/4.2.0",
+    val sdkVersion: String = "java/4.2.1",
     val osPlatform: String = "${System.getProperty("os.name")} | ${System.getProperty("java.version")}"
 )
