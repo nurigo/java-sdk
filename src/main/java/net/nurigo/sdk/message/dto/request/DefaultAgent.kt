0a8d7299d7c66d2901faf72f70950cc1528ca226
Subin Lee <chandar.sblee@gmail.com>
2025-07-31 13:00:04 +0900

    Add os architecture info in DefaultAgent.kt

--- src/main/java/net/nurigo/sdk/message/dto/request/DefaultAgent.kt
+++ src/main/java/net/nurigo/sdk/message/dto/request/DefaultAgent.kt
@@ -5,5 +5,5 @@ import kotlinx.serialization.Serializable
 @Serializable
 data class DefaultAgent(
     val sdkVersion: String = "java/4.4.0",
-    val osPlatform: String = "${System.getProperty("os.name")} | ${System.getProperty("java.version")}"
+    val osPlatform: String = "${System.getProperty("os.name")} | ${System.getProperty("java.version")} | ${System.getProperty("os.arch")}"
 )
